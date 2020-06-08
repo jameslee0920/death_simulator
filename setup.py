e4e6937bac6f3ae95f519cc15492d28c302bc69d
Thomas Winegarden <Thomas@MacBook-Pro-3.local>
2020-06-07 23:12:22 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='deathsimulator',
+      version='1.0',
+      description='Simulates how you will die',
+      author='William Wright',
+      author_email='wright.will.t@gmail.com',
+      url='https://github.com/WrightWillT/death_simulator',
+      packages=['deathsimulator'],
+     )
