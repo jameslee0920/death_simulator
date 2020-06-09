7a611436daa3de8e772c84e59cfea2a23777e462
Will Wright <wright.will.t@gmail.com>
2020-06-09 15:00:39 -0700

    started making pep8 compliant

--- death_simulator/create_death_data.py
+++ death_simulator/create_death_data.py
@@ -36,7 +36,7 @@ def combine_txt_data(filepath):
         df = combine_txt_data("../data_raw/")
     """
     df_list = []
-    for file in os.listdir("../data_raw/"):
+    for file in os.listdir(filepath):
         if file.endswith(".txt"):
             df_list.append(
                 pd.read_csv(
@@ -120,5 +120,6 @@ def create_deaths_dataset():
     deaths = deaths.drop("pred_population", axis=1)
     return deaths
 
-deaths = create_deaths_dataset()
-deaths.to_csv('../data/deaths_age_gender_race_mechanism_cause.csv', index = False)
+DEATHS_OUTPUT = create_deaths_dataset()
+DEATHS_OUTPUT.to_csv('../data/deaths_age_gender_race_mechanism_cause.csv',
+                     index=False)
